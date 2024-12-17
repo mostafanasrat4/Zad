d3045c47cff2866d9e73102db45ee6fdf96709c1
bibowskii <biboyasser3@gmail.com>
2024-12-18 01:51:26 +0200

    added a bunch of interfaces for future implementations, facilitates parallel development of controllers and models

---
+++ lib/models/interfaces/IEventRegisteration.dart
@@ -0,0 +1,15 @@
+import 'package:zad/models/classes/event_registeration.dart';
+import 'package:zad/models/classes/users.dart';
+
+abstract class IEventRegisteration{
+
+
+  Future<void> registerEvent(EventRegisteration myEventRegisteration);
+  Future<void> unregisterEvent(EventRegisteration myEventRegisteration);
+  Future<void> attendEvent(EventRegisteration myEventRegisteration);
+  Future<void> unattendEvent(EventRegisteration myEventRegisteration);
+  Future<List<User>> getEventRegisterations(String eventID);
+  Future<List<User>> getUserRegisterations(String userID);
+  Future<List<User>> getEventAttendees(String eventID);
+
+}
